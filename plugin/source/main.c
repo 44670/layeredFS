aefbb02dbd6849100e14b4311110f588749135e7
44670 <44670@44670.org>
2015-04-08 07:38:14 +0800

    bugfix

--- plugin/source/main.c
+++ plugin/source/main.c
@@ -2,6 +2,9 @@
 #include "global.h"
 #include "autogen.h"
 
+/* uncomment this to log filename into debugger output */
+// #define LOG_FILES
+
 #define O_RDONLY 1
 #define O_WRONLY 2
 #define O_RDWR   3
@@ -11,10 +14,11 @@
 #define REG16(x) (*(volatile u16*)(x))
 #define SW(addr, data)  *(u32*)(addr) = data
 
+
 Handle fsUserHandle;
 FS_archive sdmcArchive = {0x9, (FS_path){PATH_EMPTY, 1, (u8*)""}};
 
-typedef u32 (*fsRegArchiveTypeDef)(u8*, u32, u32) ;
+typedef u32 (*fsRegArchiveTypeDef)(u8*, u32, u32, u32) ;
 typedef u32 (*userFsTryOpenFileTypeDef)(u32, u16*, u32);
 typedef u32 (*fsMountArchiveTypeDef)(u32*, u32);
 
@@ -22,18 +26,21 @@ typedef u32 (*fsMountArchiveTypeDef)(u32*, u32);
 RT_HOOK	regArchiveHook;
 RT_HOOK userFsTryOpenFileHook;
 
-u32 fsRegArchiveCallback(u8* path, u32 ptr, u32 zero) {
+u32 fsRegArchiveCallback(u8* path, u32 ptr, u32 isAddOnContent, u32 isAlias) {
 	u32 ret, ret2;
 	static u32 isFisrt = 1;
 	u32 sdmcArchive = 0;
 
-	ret = ((fsRegArchiveTypeDef)regArchiveHook.callCode)(path, ptr, zero);
+	nsDbgPrint("regArchive: %s, %08x, %08x, %08x\n", path, ptr, isAddOnContent, isAlias);
+	ret = ((fsRegArchiveTypeDef)regArchiveHook.callCode)(path, ptr, isAddOnContent, isAlias);
 	if (isFisrt) {
 		isFisrt = 0;
+		
 		((fsMountArchiveTypeDef)fsMountArchive)(&sdmcArchive, 9);
 		nsDbgPrint("sdmcArchive: %08x\n", sdmcArchive);
+		
 		if (sdmcArchive) {
-			ret2 = ((fsRegArchiveTypeDef)regArchiveHook.callCode)("ram:", sdmcArchive, 0);
+			ret2 = ((fsRegArchiveTypeDef)regArchiveHook.callCode)("ram:", sdmcArchive, 0, 0);
 			nsDbgPrint("regArchive ret: %08x\n", ret2);
 		}
 	}
@@ -86,15 +93,20 @@ u32 userFsTryOpenFileCallback(u32 a1, u16 * fileName, u32 mode) {
 	u16 buf[300];
 	u32 ret;
 
+#ifdef LOG_FILES
 	convertUnicodeToAnsi(fileName, (u8*) buf);
 	nsDbgPrint("path: %s\n", buf);
+#endif
+
 	if (memcmp(ustrRom, fileName, sizeof(ustrRom)) == 0) {
 		// accessing rom:/ file
 		buf[0] = 0;
 		ustrCat(buf, ustrRootPath);
 		ustrCat(buf, &fileName[5]);
 		ret = ((userFsTryOpenFileTypeDef)userFsTryOpenFileHook.callCode)(a1, buf, 1);
+#ifdef LOG_FILES
 		nsDbgPrint("ret: %08x\n", ret);
+#endif
 		if (ret == 0) {
 			return ret;
 		}
